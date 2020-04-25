89216bd242921c20ee45b21822a664f4cbe9daa4
benoitdemaegdt <benoit.demaegdt@gmail.com>
2020-04-25 15:27:03 +0200

    add ShackListItem description

--- src/components/ShackListItem.vue
+++ src/components/ShackListItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-row class="text-center">
+  <v-row>
     <v-col cols="5">
       <template v-if="shack.images.length === 1">
         <v-img
@@ -25,7 +25,41 @@
       </template>
     </v-col>
     <v-col cols="7">
-      <p>{{ shack.name }}</p>
+      <div class="shack-type mb-1">{{ shack.type }}</div>
+      <div class="shack-name">{{ shack.name }}</div>
+      <div class="shack-divider"></div>
+      <v-row>
+        <!-- altitude -->
+        <v-col cols="5">
+          <div class="flex-container">
+            <div class="mr-3"><v-img height="25px" width="25px" :src="require('@/assets/icons/mountain.png')"></v-img></div>
+            <div class="flex-child">Altitude : {{ shack.altitude }}m</div>
+          </div>
+        </v-col>
+        <!-- beds -->
+        <v-col cols="7">
+          <div class="flex-container">
+            <div class="mr-3"><v-img height="25px" width="25px" :src="require('@/assets/icons/bed.png')"></v-img></div>
+            <div class="flex-child">Capacité : {{ shack.beds }} personnes</div>
+          </div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <!-- stove -->
+        <v-col cols="5">
+          <div class="flex-container">
+            <div class="mr-3"><v-img height="25px" width="25px" :src="require('@/assets/icons/stove.png')"></v-img></div>
+            <div class="flex-child">Poêle : {{ shack.stove ? 'oui' : 'non' }}</div>
+          </div>
+        </v-col>
+        <!-- water -->
+        <v-col cols="7">
+          <div class="flex-container">
+            <div class="mr-3"><v-img height="25px" width="25px" :src="require('@/assets/icons/water.png')"></v-img></div>
+            <div class="flex-child">Source : {{ shack.water ? 'oui' : 'non' }}</div>
+          </div>
+        </v-col>
+      </v-row>
     </v-col>
   </v-row>
 </template>
@@ -43,12 +77,36 @@ export default {
 </script>
 
 <style scoped>
+/* shack image */
 .shack-img {
   border-radius: 8px;
 }
 
 .v-carousel {
   border-radius: 8px !important;
+}
+
+/* shack info */
+.shack-type {
+  color: #71717E;
+  font-size: 14px;
+}
+.shack-name {
+  color: #222222;
+  font-size: 18px;
+}
+.shack-divider {
+  margin-top: 10px;
+  width: 32px;
+  border-top: 1px solid #DDDDDD;
+}
+
+.flex-container {
+  display: flex;
+}
+
+.flex-child {
+  flex: 1;
 }
 
 </style>
