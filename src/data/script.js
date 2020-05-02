6684070e72d7acf00e91dc2ea0fab2c3fb37bec1
benoitdemaegdt <benoit.demaegdt@gmail.com>
2020-05-02 15:16:37 +0200

    add cabanes to search bar data

--- src/data/script.js
+++ src/data/script.js
@@ -1,16 +1,60 @@
+const https = require('https');
 const fs = require('fs');
 const path = require('path');
 
 const OUTPUT = [];
+const BASE_URL = 'https://docs.google.com/spreadsheets/d/e/';
 const MASSIFS = JSON.parse(fs.readFileSync(path.resolve(__dirname, './massifs.json')));
 
-for (const massif of MASSIFS) {
-  OUTPUT.push({
-    name: massif.name,
-    type: 'massif',
-    path: `/refuges/massifs/${massif.key}`,
+(async() => {
+
+  for (const massif of MASSIFS) {
+    OUTPUT.push({
+      name: massif.name,
+      type: 'massif',
+      path: `/refuges/massifs/${massif.key}`,
+    });
+
+    const shacks = await getData(massif);
+    OUTPUT.push(...shacks);
+  }
+
+  let json = JSON.stringify(OUTPUT, null, 2);
+  fs.writeFileSync(path.resolve(__dirname, './search.json'), json);
+})()
+.catch(err => console.log('err :', err));
+
+/**
+ * download csv file
+ */
+async function getData(massif) {
+  const URL = `${BASE_URL}${massif.data.sheetId}/pub?gid=${massif.data.shackTabId}&output=csv`
+  return new Promise((resolve, reject) => {
+    let rawData = '';
+    const request = https.get(URL, res => {
+      if (res.statusCode < 200 || res.statusCode > 299) {
+        return reject(`Failed to download file: ${res.statusCode}`);
+      }
+      res.on('data', chunk => {
+        rawData += chunk;
+      });
+      res.on('end', () => {
+        rawData = rawData.toString();
+        const data = [];
+        const lines = rawData.split('\n');
+        for (let [index, line] of lines.entries()) {
+          if (index > 0) {
+            line = line.split(',');
+            data.push({
+              name: line[0],
+              type: line[2],
+              path: `/refuges/massifs/${massif.key}/cabanes/${line[1]}`,
+            });
+          }
+        }
+        return resolve(data);
+      })
+    });
+    request.on('error', err => reject(err))
   });
 }
-
-let json = JSON.stringify(OUTPUT, null, 2);
-fs.writeFileSync(path.resolve(__dirname, './search.json'), json);
