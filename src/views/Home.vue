f632b1f1f29bd165acf9782f10567a841680a196
benoitdemaegdt <benoit.demaegdt@gmail.com>
2020-05-03 12:11:30 +0200

    add 3 screens home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,17 @@
 <template>
-  <v-container fill-height fluid>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <h1 class="display-4">Home</h1>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div class="wrapper">
+    <div class="section">
+      <h1 class="display-4">Home</h1>
+    </div>
+    <div
+      class="section bg"
+      :style="{ backgroundImage: 'url(' + require('@/assets/images/home.png') + ')' }"
+    >
+    </div>
+    <div class="section">
+      <h1 class="display-4">Home</h1>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,3 +19,38 @@ export default {
   name: 'Home',
 }
 </script>
+
+<style scoped>
+.wrapper {
+  /* The height needs to be set to a fixed value for the effect to work.
+   * 100vh is the full height of the viewport. */
+  height: 100vh;
+  /* The scaling of the images would add a horizontal scrollbar, so disable x overflow. */
+  overflow-x: hidden;
+  /* Enable scrolling on the page. */
+  overflow-y: auto;
+  /* Set the perspective to 2px. This is essentailly the simulated distance from the viewport to transformed objects.*/
+  perspective: 2px;
+}
+.section {
+  /* Needed for children to be absolutely positioned relative to the parent. */
+  position: relative;
+  /* The height of the container. Must be set, but it doesn't really matter what the value is. */
+  height: 100vh;
+  
+  /* For text formatting. */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.bg {
+  /* Full height */
+  height: 100%; 
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
