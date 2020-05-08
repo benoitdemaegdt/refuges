af37f06d271738e86d0a9b7b5bf86daa984ecddf
benoitdemaegdt <benoit.demaegdt@gmail.com>
2020-05-08 16:11:07 +0200

    add new home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,52 +1,85 @@
 <template>
   <div class="wrapper">
-    <div class="section">
-      <h1 class="display-4">Home</h1>
-    </div>
-    <div
+    <v-row
       class="section bg"
+      align="center"
+      justify="center"
       :style="{ backgroundImage: 'url(' + require('@/assets/images/home.png') + ')' }"
     >
+      <v-col cols="12" sm="6">
+        <h1 class="font-weight-black display1 white--text">Votre prochaine aventure en montagne<br>commence ici</h1>
+        <v-autocomplete
+          class="mt-8"
+          v-model="searchMassif"
+          flat
+          solo
+          background-color="#EEEEEE"
+          hide-details
+          no-data-text="Ce massif sera bientôt ajouté 🙂"
+          clearable
+          prepend-inner-icon="mdi-magnify"
+          label="Chercher un massif"
+          :items="searchMassif"
+          item-text="title"
+          item-value="path"
+          return-object
+        >
+          <template v-slot:item="data">
+            <v-list-item-content>
+              <v-list-item-title v-html="data.item.title"></v-list-item-title>
+              <v-list-item-subtitle v-html="data.item.subtitle"></v-list-item-subtitle>
+            </v-list-item-content>
+          </template>
+        </v-autocomplete>
+      </v-col>
+    </v-row>
+    <!-- <div class="section">
+      <h1 class="display-4">Home</h1>
     </div>
     <div class="section">
       <h1 class="display-4">Home</h1>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
+import massifs from '@/data/massifs.json';
+
 export default {
   name: 'Home',
+  data: () => ({
+    searchMassif: massifs.map(massif => ({
+      title: massif.name,
+      subtitle: `${massif.location.region} | ${massif.location.department}`,
+      path: `/refuges/massifs/${massif.key}`,
+    })),
+  }),
+  watch: {
+    searchMassif: {
+      handler(newRoute) {
+        if (newRoute) {
+          this.$router.push({ path: newRoute.path });
+        }
+      },
+    },
+  },
 }
 </script>
 
 <style scoped>
 .wrapper {
-  /* The height needs to be set to a fixed value for the effect to work.
-   * 100vh is the full height of the viewport. */
-  height: 100vh;
-  /* The scaling of the images would add a horizontal scrollbar, so disable x overflow. */
-  overflow-x: hidden;
-  /* Enable scrolling on the page. */
-  overflow-y: auto;
-  /* Set the perspective to 2px. This is essentailly the simulated distance from the viewport to transformed objects.*/
-  perspective: 2px;
+  height: calc(100vh - 64px);
 }
 .section {
-  /* Needed for children to be absolutely positioned relative to the parent. */
-  position: relative;
-  /* The height of the container. Must be set, but it doesn't really matter what the value is. */
-  height: 100vh;
+  height: calc(100vh - 64px);
   
   /* For text formatting. */
   display: flex;
-  align-items: center;
-  justify-content: center;
+  flex-direction: column;
 }
 
 .bg {
-  /* Full height */
-  height: 100%; 
+  height: calc(100vh - 64px); 
 
   /* Center and scale the image nicely */
   background-position: center;
