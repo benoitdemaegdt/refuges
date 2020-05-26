9f63e8bb90a35cbcb19962d42efdd80f22f0ae29
benoitdemaegdt <benoit.demaegdt@gmail.com>
2020-05-26 09:06:08 +0200

    open shack view in new tab

--- src/mixins/ShackMixin.js
+++ src/mixins/ShackMixin.js
@@ -1,9 +1,8 @@
 export default {
   methods: {
     goToShack(shack) {
-      this.$router.push({ name: 'shacks', params: { massif: this.massif.key, cabane: shack.key }});
-      // const route = this.$router.resolve({ name: 'shacks', params: { massif: this.massif.key, cabane: shack.key }});
-      // window.open(route.href, '_blank');
+      const route = this.$router.resolve({ name: 'shacks', params: { massif: this.massif.key, cabane: shack.key }});
+      window.open(route.href, '_blank');
     },
   },
 };
