a06cef636f4abd0a3e1cba599af93a1243357bae
benoitdemaegdt <benoit.demaegdt@gmail.com>
2020-06-29 23:24:06 +0200

    use google sheet api v4

--- src/services/MassifService.js
+++ src/services/MassifService.js
@@ -1,46 +1,41 @@
-import Papa from 'papaparse';
+const BASE_URL = 'https://sheets.googleapis.com/v4/spreadsheets/';
+const API_KEY = process.env.VUE_APP_GOOGLE_API_KEY;
 
-const BASE_URL = 'https://docs.google.com/spreadsheets/d/e/';
-
-function getData(sheetId, gid) {
-  return new Promise((resolve, reject) => {
-    if (!sheetId || !gid) return reject('sheetId and gid cannot be undefined');
-    Papa.parse(`${BASE_URL}${sheetId}/pub?gid=${gid}&output=csv`, {
-      download: true,
-      header: true,
-      dynamicTyping: true,
-      skipEmptyLines: true,
-      transform(value, column) {
-        if (['images_indoor', 'images_outdoor', 'accesses', 'transports', 'phones'].includes(column)) {
-          return value
-            ? value.split('@@').map(element => element.trim())
-            : [];
-        }
-        return value ? value : undefined;
-      },
-      complete(results) {
-        return resolve(results.data);
-      },
-      error(error)  {
-        return reject(error);
-      },
-    });
+async function getData(sheetId, tabName) {
+  const res = await fetch(`${BASE_URL}${sheetId}/values/${tabName}?key=${API_KEY}`)
+  const data = await res.json()
+  const headers = data.values.shift();
+  return data.values.map((row) => {
+    return headers.reduce((acc, header, idx) => ({
+      ...acc,
+      [header]: castValue(header, row[idx])
+    }), {});
   });
+}
+
+function castValue(header, value) {
+  if (['images_indoor', 'images_outdoor', 'accesses', 'transports', 'phones'].includes(header)) {
+    return value ? value.split('@@').map(element => element.trim()) : [];
+  }
+  if (value === '') return undefined;
+  if (value === 'TRUE') return true;
+  if (value === 'FALSE') return false;
+  if (!isNaN(Number(value))) return Number(value);
+  return value;
 }
 
 export async function getShacksByMassif(massif) {
   try {
-    return await getData(massif.data.sheetId, massif.data.shackTabId);
+    return await getData(massif.data.sheetId, 'cabanes');
   } catch (error) {
     console.log(error);
   }
 }
 
 export async function getAllShacks() {
-  const SEARCH_SHEET_ID = '2PACX-1vR9BXRYiRrKj73d_4ddTG_NHXlHycIpbPGv8hvKmvANnUsbvTzPfWmCaV-9M7e54UcqLot7BcZKvyzO';
-  const SEARCH_TAB_ID = '1259933617';
+  const SEARCH_SHEET_ID = '1O9g4mjfmEM8rdrX1tK0jvVIoGFmBvEBDDyi3WwXy8NM';
   try {
-    return await getData(SEARCH_SHEET_ID, SEARCH_TAB_ID);
+    return await getData(SEARCH_SHEET_ID, 'cabanes');
   } catch (error) {
     console.log(error);
   }
