525aef8800e2185bac58a2e061a428b5ab196641
benoitdemaegdt <benoit.demaegdt@gmail.com>
2020-10-23 09:38:41 +0200

    add new TrekListItem component

--- src/views/TrekList.vue
+++ src/views/TrekList.vue
@@ -15,62 +15,7 @@
         <h1 class="display-2 text-center mt-6 mb-6">{{ title }}</h1>
         <v-row>
           <v-col v-for="trek in trekList" :key="trek.key" cols="12" sm="4">
-              <v-card max-width="400">
-                <v-img height="200px" :src="trek.introduction.image"></v-img>
-                <v-card-title>{{ trek.title }}</v-card-title>
-                <v-card-text class="text--primary">
-                  <v-row>
-                    <!-- distance -->
-                    <v-col cols="6" v-if="trek.summary.distance !== undefined">
-                      <div class="flex-container">
-                        <div class="mr-3"><v-img height="25px" width="25px" :src="require('@/assets/icons/distance.png')"></v-img></div>
-                        <div v-if="!isMobile" class="flex-child">Distance : {{ trek.summary.distance }}km</div>
-                        <div v-else class="flex-child">{{ trek.summary.distance }}km</div>
-                      </div>
-                    </v-col>
-                    <!-- duration -->
-                    <v-col cols="6" v-if="trek.summary.duration !== undefined">
-                      <div class="flex-container">
-                        <div class="mr-3"><v-img height="25px" width="25px" :src="require('@/assets/icons/temps.png')"></v-img></div>
-                        <div v-if="!isMobile" class="flex-child">Durée : {{ trek.summary.duration }}</div>
-                        <div v-else class="flex-child">{{ trek.summary.duration }}</div>
-                      </div>
-                    </v-col>
-                    <!-- elevation -->
-                    <v-col cols="6" v-if="trek.summary.elevation !== undefined">
-                      <div class="flex-container">
-                        <div class="mr-3"><v-img height="25px" width="25px" :src="require('@/assets/icons/mountain.png')"></v-img></div>
-                        <div class="flex-child">D+ : {{ Math.round(trek.summary.elevation) }}m</div>
-                      </div>
-                    </v-col>
-                    <!-- rating -->
-                    <v-col cols="6" v-if="trek.summary.rating !== undefined">
-                      <div class="flex-container">
-                        <div class="mr-3"><v-img height="25px" width="25px" :src="require('@/assets/icons/hook.png')"></v-img></div>
-                        <div class="flex-child">
-                          <a href="http://www.rando-marche.fr/_88181_2_les-cotations" target="_blank">Cotation : {{ trek.summary.rating }}</a>
-                        </div>
-                      </div>
-                    </v-col>
-                  </v-row>
-                </v-card-text>
-                <v-card-actions>
-                  <v-btn text @click="goToTrekDetails(trek)">Voir</v-btn>
-                  <v-spacer></v-spacer>
-                  <v-btn icon @click="showIntro = !showIntro">
-                    <v-icon>{{ showIntro ? mdiChevronUp : mdiChevronDown }}</v-icon>
-                  </v-btn>
-                </v-card-actions>
-
-                <v-expand-transition>
-                  <div v-show="showIntro">
-                    <v-divider></v-divider>
-                    <v-card-text>
-                      <p class="trek-intro">{{ trek.introduction.text }}</p>
-                    </v-card-text>
-                  </div>
-                </v-expand-transition>
-              </v-card>
+            <TrekListItem :trek="trek"/>
           </v-col>
         </v-row>
       </template>
@@ -88,21 +33,19 @@ import { getTrekList } from '@/services/TrekService.js';
 import massifs from '@/data/massifs.json';
 
 // mixin
-import LayoutMixin from '@/mixins/LayoutMixin.js';
 import ImageMixin from '@/mixins/ImageMixin.js';
 
 // components
 import TrekListSkeleton from '@/components/skeletons/TrekListSkeleton';
-
-// icons
-import { mdiChevronUp, mdiChevronDown } from '@mdi/js';
+import TrekListItem from '@/components/TrekListItem.vue'
 
 export default {
   name: 'TrekList',
   components: {
     TrekListSkeleton,
+    TrekListItem,
   },
-  mixins: [ LayoutMixin, ImageMixin ],
+  mixins: [ ImageMixin ],
   metaInfo() {
     if (this.massif && this.massif.name && this.trekList) {
       return {
@@ -127,10 +70,7 @@ export default {
     }
   },
   data: () => ({
-    mdiChevronUp,
-    mdiChevronDown,
     trekList: undefined,
-    showIntro: false,
     massif: undefined,
     isLoading: false,
   }),
@@ -157,32 +97,5 @@ export default {
       immediate: true,
     },
   },
-  methods: {
-    goToTrekDetails(trek) {
-      this.$router.push({ name: 'trekDetails', params: { massif: this.$route.params.massif, randonnee: trek.key }});
-    },
-  },
 }
 </script>
-
-<style scoped>
-.trek-intro {
-  text-align: justify;
-}
-
-.flex-container {
-  display: flex;
-}
-
-.flex-child {
-  flex: 1;
-}
-
-a {
-  text-decoration: none;
-}
-
-a:hover {
-  border-bottom: 1px solid black;
-}
-</style>
